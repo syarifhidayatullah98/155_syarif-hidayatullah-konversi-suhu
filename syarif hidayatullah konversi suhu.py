034b744ff9a5713fe66f861a026d98c6d7a30348
syarifhidayatullah98 <143860057+syarifhidayatullah98@users.noreply.github.com>
2023-09-10 13:47:47 +0700

    Add files via upload

---
+++ syarif hidayatullah konversi suhu.py
@@ -0,0 +1,46 @@
+NAMA = str(input("masukan nama kamu = "))
+print(NAMA, "nama saya syarif hidayatullah")
+print("nim 230441100155")
+
+
+
+
+# 1
+
+celcius = float(input("Masukan suhu celcius = "))
+kelvin = (celcius + 273.15)
+print("jadi suhu kelvim adalah: ", kelvin, "derajat")
+
+# 2
+
+celcius = float(input("Masukan suhu celcius = "))
+fahrenheit = (celcius * 9/5) + 32
+print("jadi suhu fahrenheit adalah: ", fahrenheit, "derajat")
+
+# 3
+
+fahrenheit = float(input("Masukan suhu fahrenheit = "))
+celcius = (fahrenheit - 32) * 5/9
+print("jadi suhu celcius adalah: ", celcius, "derajat")
+
+# 4
+
+fahrenheit = float(input("Masukan suhu fahrenheit = "))
+kelvin = (fahrenheit - 32) * 5/9 + 273.15
+print("jadi suhu kelvin adalah: ", kelvin, "derajat")
+
+# 5
+
+kelvin = float(input("Masukan suhu kelvin = "))
+celcius = (kelvin - 273.15)
+print("jadi suhu celciust adalah: ", celcius, "derajat")
+
+# 6
+
+kelvin = float(input("Masukan suhu kelvin = "))
+fahrenheit = (kelvin - 273.15) * 9/5 + 32
+print("jadi suhu fahrenheit adalah: ", fahrenheit, "derajat")
+
+
+
+
